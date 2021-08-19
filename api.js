a9772acb240ae2d58501264cd7f696c9e2f6e6d1
leo <leonardobcarv@outlook.com>
2021-08-19 02:55:32 -0300

    Fix Json name

--- api.js
+++ api.js
@@ -2,112 +2,112 @@
     {"id": 1,
     "team": "Barcelona",
     "description": "Barcelona Squad",
-    "avg-age": "23.2",
-    "player-rate": ""
+    "ageAveragee": "23.2",
+    "playerRate": ""
     },
 
     {"id": 2,
     "team": "Real Madrid",
     "description": "Real Madrid Squad",
-    "avg-age": "23,8",
-    "player-rate": ""
+    "ageAveragee": "23,8",
+    "playerRate": ""
     },
 
     {"id": 3,
     "team": "Milan",
     "description": "Milan Squad",
-    "avg-age": "28.0",
-    "player-rate": ""
+    "ageAveragee": "28.0",
+    "playerRate": ""
     },
 
     {"id": 4,
     "team": "Liverpool",
     "description": "Liverpool Squad",
-    "avg-age": "28.2",
-    "player-rate": ""
+    "ageAveragee": "28.2",
+    "playerRate": ""
     },
 
     {"id": 5,
     "team": "Bayern Munich",
     "description": "Bayern Munich Squad",
-    "avg-age": " 25,7",
-    "player-rate": ""
+    "ageAveragee": " 25,7",
+    "playerRate": ""
     },
 
     {"id": 6,
     "team": "Lazio",
     "description": "Lazio Squad",
-    "avg-age": "27,2",
-    "player-rate": ""
+    "ageAveragee": "27,2",
+    "playerRate": ""
     },
 
     {"id": 7,
     "team": "Inter Milan",
     "description": "Inter Milan Squad",
-    "avg-age": "27,9",
-    "player-rate": ""
+    "ageAveragee": "27,9",
+    "playerRate": ""
     },
 
     {"id": 8,
     "team": "APOEL Nicosia",
     "description": "APOEL Nicosia Squad",
-    "avg-age": "24,7",
-    "player-rate": ""
+    "ageAveragee": "24,7",
+    "playerRate": ""
     },
 
     {"id": 9,
     "team": "AC Milan",
     "description": "AC Milan Squad",
-    "avg-age": "25,8",
-    "player-rate": ""
+    "ageAveragee": "25,8",
+    "playerRate": ""
     },
 
     {"id": 10,
     "team": "Besiktas JK",
     "description": "Besiktas JK Squad",
-    "avg-age": "27,1",
-    "player-rate": ""
+    "ageAveragee": "27,1",
+    "playerRate": ""
     },
 
     {"id": 11,
     "team": "Olympiacos Piraeus",
     "description": "Olympiacos Piraeus Squad",
-    "avg-age": "24,4",
-    "player-rate": ""
+    "ageAveragee": "24,4",
+    "playerRate": ""
     },
 
     {"id": 12,
     "team": "Zalgiris Vilnius",
     "description": "Zalgiris Vilnius Squad",
-    "avg-age": "26,3",
-    "player-rate": ""
+    "ageAveragee": "26,3",
+    "playerRate": ""
     },
 
     {"id": 11,
     "team": "Arsenal FC",
     "description": "Arsenal FC Squad",
-    "avg-age": "25,9",
-    "player-rate": ""
+    "ageAveragee": "25,9",
+    "playerRate": ""
     },
 
     {"id": 11,
     "team": "Ajax Amsterdan",
     "description": "Ajax Amsterdan Squad",
-    "avg-age": "25,6",
-    "player-rate": ""
+    "ageAveragee": "25,6",
+    "playerRate": ""
     },
     
     {"id": 11,
     "team": "FC Nantes",
     "description": "FC Nantes Squad",
-    "avg-age": "26,2",
-    "player-rate": ""
+    "ageAveragee": "26,2",
+    "playerRate": ""
     },
         
     {"id": 11,
     "team": "CSKA Moscow",
     "description": "CSKA Moscow Squad",
-    "avg-age": "25,0",
-    "player-rate": ""
+    "ageAveragee": "25,0",
+    "playerRate": ""
     }
 ]
