ce1b0bfa12518c6051e7f083c2f39452ffad1e96
leo <leonardobcarv@outlook.com>
2021-08-19 23:38:46 -0300

    Revert BUG

--- api.js
+++ api.js
@@ -3,95 +3,111 @@
     "team": "Barcelona",
     "description": "Barcelona Squad",
     "ageAverage": 23.2,
+    "playerRate": ""
     },
 
     {"id": 2,
     "team": "Real Madrid",
     "description": "Real Madrid Squad",
     "ageAverage": 23.8,
+    "playerRate": ""
     },
 
     {"id": 3,
     "team": "Milan",
     "description": "Milan Squad",
     "ageAverage": 28.0,
+    "playerRate": ""
     },
 
     {"id": 4,
     "team": "Liverpool",
     "description": "Liverpool Squad",
     "ageAverage": 28.2,
+    "playerRate": ""
     },
 
     {"id": 5,
     "team": "Bayern Munich",
     "description": "Bayern Munich Squad",
     "ageAverage": 25.7,
+    "playerRate": ""
     },
 
     {"id": 6,
     "team": "Lazio",
     "description": "Lazio Squad",
     "ageAverage": 27.2,
+    "playerRate": ""
     },
 
     {"id": 7,
     "team": "Inter Milan",
     "description": "Inter Milan Squad",
     "ageAverage": 27.9,
+    "playerRate": ""
     },
 
     {"id": 8,
     "team": "APOEL Nicosia",
     "description": "APOEL Nicosia Squad",
     "ageAverage": 24.7,
+    "playerRate": ""
     },
 
     {"id": 9,
     "team": "AC Milan",
     "description": "AC Milan Squad",
     "ageAverage": 25.8,
+    "playerRate": ""
     },
 
     {"id": 10,
     "team": "Besiktas JK",
     "description": "Besiktas JK Squad",
     "ageAverage": 27.1,
+    "playerRate": ""
     },
 
     {"id": 11,
     "team": "Olympiacos Piraeus",
     "description": "Olympiacos Piraeus Squad",
     "ageAverage": 24.4,
+    "playerRate": ""
     },
 
     {"id": 12,
     "team": "Zalgiris Vilnius",
     "description": "Zalgiris Vilnius Squad",
     "ageAverage": 26.3,
+    "playerRate": ""
     },
 
     {"id": 13,
     "team": "Arsenal FC",
     "description": "Arsenal FC Squad",
     "ageAverage": 25.9,
+    "playerRate": ""
     },
 
     {"id": 14,
     "team": "Ajax Amsterdan",
     "description": "Ajax Amsterdan Squad",
     "ageAverage": 25.6,
+    "playerRate": ""
     },
     
     {"id": 15,
     "team": "FC Nantes",
     "description": "FC Nantes Squad",
     "ageAverage": 26.2,
+    "playerRate": ""
     },
         
     {"id": 16,
     "team": "CSKA Moscow",
     "description": "CSKA Moscow Squad",
     "ageAverage": 25.0,
+    "playerRate": ""
     }
 ]
